c2f0ebc544fa7a99dbe6d6e5688f8601f6647a8a
Arel <32299070+alitunaarel@users.noreply.github.com>
2020-08-12 23:21:07 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ $(document).ready(function () {
     $('.selection.dropdown')
         .dropdown();
 
+    $('.ui.form')
+        .form({
+            fields: {
+                lastname: 'empty',
+                firstname: 'empty',
+                gender: 'empty',
+                email: 'empty',
+                password: ['minLength[6]', 'empty'],
+                terms: 'checked',
+                terms2: 'empty'
+            }
+        })
+        ;
+
 });
 
-console.log('azeaz')
+console.log('test')
